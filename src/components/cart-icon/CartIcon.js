326135d1d5572200f5bd621b34d3fe8fc831f9ba
DamjanKarlovski <damjan_karlovski@hotmail.com>
2021-04-27 12:12:45 +0200

    added github pages

--- src/components/cart-icon/CartIcon.js
+++ src/components/cart-icon/CartIcon.js
@@ -3,11 +3,12 @@ import { connect } from 'react-redux';
 import {toggleCartHidden } from '../../redux/cart/cart.actions'
 import { ReactComponent as ShopppingIcon } from '../../assets/shopping-bag.svg';
 import './CartIcon.styles.scss';
+import CartItem from '../cart-item/Cart-Item';
 
-const CartIcon = ({toggleCartHidden}) => (
+const CartIcon = ({toggleCartHidden, itemCount}) => (
     <div className="cart-icon" onClick={toggleCartHidden}>
         <ShopppingIcon className="shopping-icon"/>
-        <span className="item-count">0</span>
+        <span className="item-count">{itemCount}</span>
     </div>
 )
 
@@ -15,4 +16,10 @@ const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 })
 
-export default connect(null, mapDispatchToProps)(CartIcon)
+const mapStateToProps = ({ cart: {cartItems }}) => ({
+    itemCount: cartItems.reduce(
+        (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity,
+         0)
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon)
